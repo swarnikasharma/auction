5e001eb9d752de28a1873a3d95ecde3b5ea28185
swarnikasharma <swarnikasharma1102@gmail.com>
2018-09-22 02:14:50 +0530

    error handling added and data structures modified

--- Auction.sol
+++ Auction.sol
@@ -1,3 +1,4 @@
+pragma experimental ABIEncoderV2;
 pragma solidity ^0.4.24;
 contract Auction{
     address public addr_auctioneer;
@@ -5,25 +6,27 @@ contract Auction{
     uint256 public q;   //large prime
     uint256 public n_bid_items;
     uint256 public item_index;
-    uint256 public t;    //for testing
-    
+    int256 public t;    //for testing
+    int256 public count_notary;
+    bool public auction_open;
+    int256 public count_bidder;
     struct bidder{
         uint256 n_items;
-        uint256 u_w;
-        uint256 v_w;
+        int256 u_w;
+        int256 v_w;
         uint index;
-        uint256[2][] items;
+        int256[2][] items;
     }
     struct notary{
         address addr_notary;
         uint256 flag;
-        
+        int256 pay;
     }
     
     mapping(address => bidder) public bidders;
-    mapping(uint256 => address) public assigned_notary;  //notary assigned to bidder
+    mapping(int256 => address) public assigned_notary;  //notary assigned to bidder
     mapping(uint256 => uint256) public bid_items;      //items for auction
-    mapping(uint256 => notary) public notaries;
+    mapping(int256 => notary) public notaries;
     
     //constructor
     function Auction(uint256 prime, uint256 n)
@@ -32,76 +35,67 @@ contract Auction{
         payment=0;
         q=prime;
         n_bid_items=n;
+        auction_open=false;
+        count_bidder = 0;
         //t=msg.sender;
     }
     //adding items to the main item list which are open for auction. 
     //Also making a check so that the current index does not exceed the total item count. 
     function add_bid_items(uint256 item_id)
     {
-        if(item_index<n_bid_items)
-        {
-            bid_items[item_index]=item_id;
-            item_index=item_index+1;
-        }
+           if(item_index<n_bid_items)
+            {
+                bid_items[item_index]=item_id;
+                item_index=item_index+1;
+            }
+            else
+            {
+                auction_open = true;
+            }
+        
     }
     //functions that registers notary
-    function register_notary(uint256 id)
+    function register_notary()
     {
-        notaries[id].addr_notary=msg.sender;
-        notaries[id].flag=0;
+        notaries[count_notary].addr_notary=msg.sender;
+        notaries[count_notary].flag=0;
+        count_notary++;
+        
     }
     //function that registers bidder
-    function register_bidder(uint256 u_w,uint256 v_w,uint n_items)
+    function register_bidder(int256 u_w,int256 v_w,uint n_items)
     {
+        count_bidder++;
         bidders[msg.sender].u_w=u_w;
         bidders[msg.sender].v_w=v_w;
         bidders[msg.sender].n_items=n_items;
         bidders[msg.sender].index=0;
         //randomly assign a notary to a bidder
-        uint256 r;
+        int256 r;
         do
         {
-            r=uint256(keccak256(block.timestamp, block.difficulty))%q;
+             r = int(block.blockhash(block.number-1))%10 + 1;
+            //r=uint64(keccak256(block.timestamp, block.difficulty))%(q);
         }while(notaries[r].flag==1);
         assigned_notary[r]=msg.sender;
         notaries[r].flag=1;
     }
     //function to add items that the bidder wants to bid on 
-    function addItem(uint256 u,uint256 v)
+    function addItem(int256 u,int256 v)
     {
-        if(bidders[msg.sender].index<bidders[msg.sender].n_items)
+        if(auction_open)
         {
-            bidders[msg.sender].items.push([u,v]);
-            bidders[msg.sender].index++;
+            if(bidders[msg.sender].index<bidders[msg.sender].n_items)
+            {
+                bidders[msg.sender].items.push([u,v]);
+                bidders[msg.sender].index++;
+            }
         }
         
     }
-    //sorting
-//   function sort()
-//     {
-       
-//         quickSort(data, 0, data.length - 1);
-//     }
-    // function quickSort(uint[] storage arr, uint left, uint right) internal 
-    // {
-    //     uint i = left;
-    //     uint j = right;
-    //     uint pivot = arr[left + (right - left) / 2];
-    //     while (i <= j) {
-    //         while (arr[i] < pivot) i++;
-    //         while (pivot < arr[j]) j--;
-    //         if (i <= j) {
-    //             (arr[i], arr[j]) = (arr[j], arr[i]);
-    //             i++;
-    //             j--;
-    //         }
-    //     }
-    //     if (left < j)
-    //         quickSort(arr, left, j);
-    //     if (i < right)
-    //         quickSort(arr, i, right);
-    // }
-
+    
+   
+   
     //only for testing purpose
     function test() 
     {
